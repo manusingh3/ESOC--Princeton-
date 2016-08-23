29355173402d1bb8324b860d3fba714c5b991d0f
Manu Singh <manusingh3@gmail.com>
2016-08-23 15:32:07 -0400

    Update Health_DHS.R

--- Health Indicators/Health_DHS.R
+++ Health Indicators/Health_DHS.R
@@ -1,1 +1,23 @@
-# Placeholder
+# Health indicators from DHS files
+library(foreign)
+
+file = file.choose()
+#file = "C:\\Users\\ms52\\Dropbox (ESOC - Princeton)\\ESOC\\Afghanistan\\OAPA Lessons Learned Study\\Raw Data\\DHS\\afch66fl\\UNDER5.DAT"
+under5 = read.dta(file.choose())
+
+a = attr(under5,"var.labels")
+colnames(under5) = a
+head(under5)
+write.csv(under5, file = "dealth_other.csv")
+
+b = (attr(under5, "label"))
+b = as.data.frame(unlist(b)
+
+                  
+######Calculate maternal mortality rate use data women.csv##################                 
+df = read.csv(file.choose())
+head(df)
+a = colnames(df)
+sibs = df[,1149:1689]
+id = df[,1:40]
+sibs <- sibs[,colSums(is.na(sibs))<nrow(sibs)] ##### remove columns where all the values are NA
