7a2a09e304acb66e8bfd33b519b4dacba589db9b
Manu Singh <manusingh3@gmail.com>
2016-09-16 10:58:51 -0400

    nightlights correlated with survey datasets

---
+++ cor_nightlights.R
@@ -0,0 +1,89 @@
+b = aggregate(d$MISTI_W1_D9~ d$name, FUN = "sum")
+
+d = read.csv(file.choose())
+
+nrva = read.csv(file.choose())
+nrva = na.omit(nrva)
+
+colnames(b ) = c("PROV_34_NA", "misti")
+temp = merge(d, b , by = "PROV_34_NA")
+
+trial = aggregate(temp$PROV_34_NA~ temp$misti, FUN = "mean")
+
+nrva = read.csv(file.choose())
+c = aggregate(nrva$nrva~ nrva$Province_Name, FUN = "sum")
+colnames(c) = c("PROV_34_NA", "nrva")
+
+temp = merge(d , c, by = "PROV_34_NA")
+
+b = aggregate(temp$PROV_34_NA~temp$tot_rad, FUN = "sum")
+temp$PROV_34_NA = as.factor(temp$PROV_34_NA)
+
+b = aggregate(d$norm_rad~d$PROV_34_NA, FUN = "mean"); colnames(b) = c("prov", "rad")
+c = aggregate(nrva$nrva~nrva$Province_Name, FUN = "sum" ) ;colnames(c) = c("prov", "nrva")
+
+
+
+# c$prov = ifelse(c$prov == "Sar-e-Pul", "Sari Pul", c$prov)
+# c$prov = ifelse(c$prov == "Sar-e-Pul", "Sari Pul", c$prov)
+
+c$prov = as.character(c$prov)
+
+c$prov[c$prov =="Sar-e-Pul"] = "Sari Pul"
+c$prov[c$prov =="Urozgan"] = "Uruzgan"
+c$prov[c$prov =="Helmand"] = "Hilmand"
+c$prov[c$prov =="Herat"] = "Hirat"
+c$prov[c$prov =="Kunarha"] = "Kunar"
+
+
+
+
+table(c$prov);table(b$prov)
+
+
+
+total = merge(b, c , by = "prov")
+
+cor(total$rad, total$nrva)
+
+misti = read.csv(file.choose())
+
+total = merge(total, misti, by = "prov")
+
+dhs = read.csv(file.choose())
+head(dhs)
+colnames(dhs) = c("wi_country", "q_country", "wi_urban", "q_urban","wi_rural", "q_rural", "prov")
+
+colnames(dhs) = c("prov", "dhs")
+table(b$prov);table(dhs$prov)
+
+test$prov = as.character(test$prov)
+
+test$prov[test$prov =="helmand"] = "hilmand"
+test$prov[test$prov =="herat"] = "hirat"
+test$prov[test$prov =="urozgan"] = "uruzgan"
+
+
+test = aggregate(df$wi_country~df$prov, FUN = "mean")
+test.1 = aggregate(df$wi_urban~df$prov, FUN = "mean")
+
+test = merge( test.1,test, by = "df$prov", all = TRUE)
+colnames(test) = c("prov", "urban","rural", "country")
+table(b$prov);table(test$prov)
+dhs.cor = merge(test, b, by = "prov")
+write.csv(dhs.cor, file = "dhs_cor.csv")
+
+#######DHS 
+
+rownames(dhs.cor) = dhs.cor$prov
+ dhs.cor = dhs.cor[, -1]
+dhs.cor
+
+dhs.cor = data.matrix(dhs.cor)
+
+dhs <- heatmap(dhs.cor, Rowv=NA, Colv=NA, col = cm.colors(256), scale="column", margins=c(5,10))
+
+m = cor(dhs.cor, use="pairwise.complete.obs")
+
+library(corrplot)
+corrplot(m, method="pie")
